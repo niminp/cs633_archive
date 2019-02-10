ce2ce087dcbf42c302516de31c16f7d719333a2d
Nimin Patel <niminp@bu.edu>
2019-02-10 11:53:21 -0500

    Create Language Model

--- manager/models.py
+++ manager/models.py
@@ -24,6 +24,7 @@ class Project(models.Model):
 	facilitator = models.ManyToManyField(User, related_name='projects')
 	team_member = models.ManyToManyField('TeamMember', related_name='projects')
 	keyword = models.ManyToManyField('Keyword', related_name='projects')
+	language = models.ManyToManyField('Language', related_name='projects')
 	github = models.URLField(null=True, blank=True)
 	project_management = models.URLField(null=True, blank=True)
 	website = models.URLField(null=True, blank=True)
@@ -36,7 +37,7 @@ class Project(models.Model):
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
-	fields = ['name', 'description', 'year', 'semester', 'instructor', 'facilitator', 'team_member', 'keyword', 'github', 'project_management', 'website', 'is_hidden', 'last_modified', 'created']
+	fields = ['name', 'description', 'year', 'semester', 'instructor', 'facilitator', 'team_member', 'keyword', 'language', 'github', 'project_management', 'website', 'is_hidden', 'last_modified', 'created']
 	readonly_fields = ['last_modified', 'created']
 	list_display = ['id', 'name', 'description', 'year', 'semester', 'instructor_link', 'facilitator_link', 'github', 'project_management', 'website', 'is_hidden', 'last_modified', 'created']
 	list_filter = ['is_hidden', 'instructor', 'facilitator', 'team_member', 'keyword', 'semester', 'year']
@@ -86,3 +87,18 @@ class KeywordAdmin(admin.ModelAdmin):
 	readonly_fields = ['last_modified', 'created']
 	list_display = ['id', 'word', 'last_modified', 'created']
 	search_fields = ['word']
+
+class Language(models.Model):
+	name = models.CharField(max_length=255)
+	created = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+	last_modified = models.DateTimeField(auto_now=True)
+
+	def __str__(self):
+		return "{}".format(self.name)
+
+@admin.register(Language)
+class LanguageAdmin(admin.ModelAdmin):
+	fields = ['name',  'last_modified', 'created']
+	readonly_fields = ['last_modified', 'created']
+	list_display = ['id', 'name', 'last_modified', 'created']
+	search_fields = ['name']
